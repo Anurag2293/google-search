937253888d6d6771f8660d51d5a48d9a9be67414
Anurag Dhote <anuragdhote392@gmail.com>
2023-12-21 23:10:40 +0530

    styling home page 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 
+import SearchBar from '../components/SearchBar';
+
 const Home = () => {
 	const navigate = useNavigate();
 	const [searchQuery, setSearchQuery] = useState('');
 
     return (
-        <div>
-            <h1>Google Search</h1>
-			<input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-			<button onClick={() => navigate(`/search?q=${searchQuery.split(' ').join('+')}`)}>Submit</button>
-			<p>{searchQuery}</p>
+        <div className='home-container'>
+            <h1 id="product-sans">Google</h1>
+            <div className='home-search-bar'>
+                <input className='home-search-input-bar' type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                <button onClick={() => navigate(`/search?q=${searchQuery.split(' ').join('+')}`)}>Submit</button>
+            </div>
+			{/* <p>{searchQuery}</p> */}
+            <SearchBar />
         </div>
     )
 }
